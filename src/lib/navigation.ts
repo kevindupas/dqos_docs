ebfda3bc7d0172a37b55ae7a6af1f152fa97971f
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2025-06-02 16:03:16 +0200

    Initial commit

---
+++ src/lib/navigation.ts
@@ -0,0 +1,111 @@
+export const navigation = [
+  {
+    title: 'Introduction',
+    links: [
+      { title: 'DQoS Overwiew', href: '/' },
+      // { title: 'Installation', href: '/docs/installation' },
+    ],
+  },
+  {
+    title: 'Login',
+    links: [
+      { title: 'Login', href: '/docs/login' },
+      {
+        title: 'Reset Password',
+        href: '/docs/reset-password',
+      },
+      { title: 'User Profile', href: '/docs/user-profile' },
+      {
+        title: 'Two-Factor Authentication',
+        href: '/docs/two-factor-authentication',
+      },
+    ],
+  },
+  {
+    title: 'NMS Views',
+    links: [
+      { title: 'PM Data Availability', href: '/docs/pm-data-availability' },
+      { title: 'RI Capture Duration', href: '/docs/ri-capture-duration' },
+      { title: 'NMS Summary', href: '/docs/nms-summary' },
+      { title: 'NMS Details', href: '/docs/nms-details' },
+      { title: 'NMS Benchmark', href: '/docs/nms-benchmark' },
+      {
+        title: 'Scoring',
+        href: '/docs/scoring',
+      },
+      {
+        title: 'Ranking',
+        href: '/docs/ranking',
+      },
+      {
+        title: 'Coverage VS QoS',
+        href: '/docs/coverage-vs-qos',
+      },
+      {
+        title: 'Antennas Map',
+        href: '/docs/antennas-map',
+      },
+    ],
+  },
+  {
+    title: 'User Feedback',
+    links: [
+      { title: 'Dashboard Application Installation', href: '/docs/dashboard-application-installation' },
+      { title: 'User Complaints', href: '/docs/user-complaints' },
+      { title: 'Follow Up Complaints', href: '/docs/follow-up-complaints' },
+      { title: 'Surveys', href: '/docs/surveys' },
+      { title: 'Notifications', href: '/docs/notifications' },
+      { title: 'Imports Page', href: '/docs/imports-page' },
+      { title: 'Location Manager', href: '/docs/location-manager' },
+      { title: 'Translation Manager', href: '/docs/translation-manager' },
+      { title: 'FAQ Manager', href: '/docs/faq-manager' },
+    ],
+  },
+  {
+    title: 'Reporting Builder',
+    links: [
+      { title: 'Generate Template', href: '/docs/generate-template' },
+      { title: 'Generate campaign', href: '/docs/generate-campaign' },
+      { title: 'Architecture guide', href: '/docs/architecture-guide' },
+      { title: 'Export Report', href: '/docs/export-report' },
+    ],
+  },
+  {
+    title: 'Support tickets',
+    links: [
+      { title: 'Ticket Management', href: '/docs/ticket-management' },
+      { title: 'Ticket Creation', href: '/docs/ticket-creation' },
+      { title: 'Ticket Tracking', href: '/docs/ticket-tracking' },
+      { title: 'Ticket Email Notifications', href: '/docs/ticket-email-notifications' },
+    ],
+  },
+  {
+    title: 'Data Management',
+    links: [
+      { title: 'Topology Update Assistant', href: '/docs/topology-update-assistant' },
+      { title: 'Operator Management', href: '/docs/operator-management' },
+      { title: 'Problem Management', href: '/docs/problem-management' },
+      { title: 'Notification Manager', href: '/docs/notification-manager' },
+    ],
+  },
+  {
+    title: 'User Management',
+    links: [
+      { title: 'Roles', href: '/docs/roles' },
+      { title: 'Permissions', href: '/docs/permissions' },
+      { title: 'Users', href: '/docs/users' },
+    ],
+  },
+  {
+    title: 'Settings',
+    links: [
+      { title: 'General', href: '/docs/general-settings' },
+      { title: 'Map', href: '/docs/map-settings' },
+      { title: 'Kpi\'s', href: '/docs/kpi-settings' },
+      { title: 'Coverage', href: '/docs/coverage-settings' },
+      { title: 'Scoring', href: '/docs/scoring-settings' },
+      { title: 'Ri Settings', href: '/docs/ri-settings' },
+      { title: 'Data Source', href: '/docs/data-source-settings' },
+    ],
+  },
+]
